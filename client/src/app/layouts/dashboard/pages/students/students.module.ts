a16c6ae44d0d9209c71a55c098bc2ec079cbba8e
KariRomero <karinaromero.1993@gmail.com>
2024-04-12 20:59:38 -0300

    created students and students-dialog

--- client/src/app/layouts/dashboard/pages/students/students.module.ts
+++ client/src/app/layouts/dashboard/pages/students/students.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentsComponent } from './students.component';
 import { SharedModule } from '../../../../shared/shared.module';
+import { StudentsDialogComponent } from './students-dialog/students-dialog.component';
 
 
 @NgModule({
   declarations: [
-    StudentsComponent
+    StudentsComponent,
+    StudentsDialogComponent
   ],
   imports: [
     CommonModule,
